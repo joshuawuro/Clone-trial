0f07c7b532beaa1af2f15bd925330bf3328667c6
Joshuawuro <joshuawuro@gmail.com>
2024-09-03 18:25:52 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-gray-200 rounded shadow w-1/2 p-3 mx-auto mt-6">
+        <h1 className="font-bold py-2">Your Name!</h1>
+        <p className="text-xs mb-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          adipisci voluptatem tenetur.
+        </p>
+        <button className=" text-white px-2 py-1.5 font-md text-sm my-2 bg-blue-600 rounded-lg hover:bg-blue-500 cursor:pointer">
+          Learn More ↗️
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default App;
